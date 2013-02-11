79d062fb71022d964b6fa88252b073e39a9f4ff5
sddhrthrt <sddhrthrt@gmail.com>
2013-02-11 08:05:45 +0530

    changed

--- tablet/scratchpad.js
+++ tablet/scratchpad.js
@@ -11,7 +11,67 @@ $(function(){
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
     $(".thumbnail").each(function(index){
-        $(this).append('<span class="ui-icon ui-icon-pencil pull-right"></span>');
+        $(this).append('<span class="edit ui-icon ui-icon-pencil pull-right"></span>');
     });
+    function showMenu($this){
+        $("#edit-icon").animate({opacity: 0.2}, 10);
+        $(".pulldown").remove();
+        $("<div>").addClass("pulldownmenu").appendTo(".editor");
+        var items = ["Brightness", "Saturation",
+                    "Exposure", "Sepia",
+                    "Noise", "Contrast",
+                    "Vibrance", "Hue",
+                    "Gamma", "Clip"];
+        var list = $("<div>").addClass("menuitems");
+        for(var item in items){
+            list.append($("<div class='menuitem rangeeffect'>"+items[item]+"</div>").attr("id", items[item]));
+        }
+        $("<div>").addClass("saveclosemenu menuitem").append("<i class='icon-ok'></i>").appendTo(list);
+        $("<div>").addClass("closemenu menuitem").append("<i class='icon-undo'></i>").appendTo(list);
+        list.appendTo('.pulldownmenu');
+        $(".rangeeffect").mouseover(function(){
+            var $this = $(this),
+                $id = $this.attr("id"),
+                $range = $this.children({id: $id});
+            $this.siblings().each(function(){
+                $(this).children(".rangeholder").remove();
+            })
+            if($range.length==0){
+                $this.append("<div class='rangeholder'><input type='range' id="+$id+"range></div>");
+            }
+        });
+        $(".saveclosemenu").click(function(){
+            $(".pulldownmenu").remove();
+            showMenuButton();
+        });
+        $(".closemenu").click(function(){
+            $(".pulldownmenu").remove();
+            showMenuButton();
+        });
+
+    }
+    function showMenuButton(){
+        $("<div>").addClass("pulldown").appendTo(".editor");
+        $("<i class='icon-edit' id='edit-icon'></i>").appendTo(".pulldown");
+        $(".pulldown").click(function(event){
+            showMenu();
+        });
+    }
+	function edit($item){
+		$("<div class='editor'></div>").appendTo($(".screen"));
+		$(".gallery").hide();
+		var src = $item.attr("href");
+		$("<img>").addClass("editable").attr("src", src).appendTo(".editor");
+        showMenuButton();
+	}
+	$(".thumbnail").click(function(event){
+		var $item = $(this),
+			$target = $(event.target);
+
+		if($target.is(".edit")){
+			edit($item);
+		}
+	});
+
 });
 
